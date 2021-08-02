e5ab09386c163163c156e507201e8bc448310900
Ryan Zhu <ryanzhu1024@outlook.com>
2021-08-02 16:26:27 -0700

    init

---
+++ packages/formengine/src/formInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from 'ui-components/textField'
+
+const FormInput = () => {
+  return (
+    <div>
+      <TextField />
+    </div>
+  )
+}
+
+export default FormInput
---
+++ packages/ui-components/src/textField/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextField = () => {
+  return (
+    <div>
+      1232
+    </div>
+  )
+}
+
+export default TextField
